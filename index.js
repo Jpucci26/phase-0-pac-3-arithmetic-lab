c9cfeb96ce5c199c08250b62af3d1b600dff4f93
Jake <jakepucci26@gmail.com>
2022-06-16 10:23:23 -0600

    commit tree 3d6182062c4fca5dc6ccff802f5a8c4af7e4b1d3

---
+++ index.js
@@ -0,0 +1,24 @@
+function add(a, b) {
+    return a+b;
+}
+function subtract(a, b) {
+    return a-b;
+}
+function multiply(a, b){
+    return a*b;
+}
+function divide(a, b){
+    return a/b;
+}
+
+function increment(n) {n++
+return n++};
+
+function decrement(n) {n--
+return n--}
+
+function makeInt(n) {parseInt("assume base 10", 10); 
+return parseInt(n, 10)}; // isn't base 10 already assumed?
+
+function preserveDecimal(n) {parseFloat("2.222");
+return parseFloat(n)};
